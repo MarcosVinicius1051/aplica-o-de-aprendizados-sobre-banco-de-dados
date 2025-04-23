f65be1cbb3b51bd5821986a6535a6de86190217b
MarcosVinicius1051 <marcosluxvinicius@gmail.com>
2025-04-23 10:57:56 -0300

    pausa para estudos sobre rotas e backend

---
+++ app.js
@@ -0,0 +1,24 @@
+import exp from "constants";
+import express from "express";
+import path from "path";
+
+export const app = express();
+export const __dirname = path.resolve(); 
+const port = 8080;
+
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname + "/index.html")
+})
+
+
+app.listen(port, ()=>{
+  console.log("servidor ligado")
+})
+
+
+
+
+
+
+
+
